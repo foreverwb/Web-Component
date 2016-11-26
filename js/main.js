0f931313527c13d05b5a3436d263cac566f4f7df
foreverwb <504543582@qq.com>
2016-11-26 18:32:59 +0800

    DTY

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ require.config({
 require(['jquery', 'window'], function($, w) {
     $("#a").click(function() {
          new w.Window().alert({
+            textAlterBtn : "确定",
+            skinClassName : null,
+            hasCloseBtn : false,
+            title : "提示",
             content : "Hello",
             handler : function() {
                 alert("you click the button");
--- js/window.js
+++ js/window.js
@@ -4,18 +4,26 @@ define(['jquery'], function($) {
             width : 500,
             height : 300,
             content : "",
-            handler : null
+            handler : null,
+            title : "系统消息",
+            hasCloseBtn : true,
+            skinClassName : null,
+            textAlterBtn : "OK"
         };
     }
 
     Window.prototype = {
         alert : function(cfg) {
                         var CFG =  $.extend(this.cfg, cfg);
-                        var boundingBox = $('<div class="window_boundingBox"></div>');
+                        var boundingBox = $(
+                            '<div class="window_boundingBox">' +
+                                '<div class="window_header"> '+ CFG.title +' </div>' +
+                                '<div class="window_content"> '+ CFG.content + '</div>' +
+                                '<div class="window_footer"><input type="button" value=" '+ CFG.textAlterBtn +'"></div>' +
+                            '</div>'
+                        ),
+                        btn = boundingBox.find(".window_footer input");
                         boundingBox.appendTo("body");
-                        boundingBox.html(CFG.content);
-                        var btn = $('<input type="button" value="确定">');
-                        btn.appendTo(boundingBox);
                         btn.click(function() {
                             CFG.handler && CFG.handler();
                             boundingBox.remove();
@@ -26,6 +34,16 @@ define(['jquery'], function($) {
                             left : (CFG.x || (window.innerWidth - CFG.width) / 2) + "px",
                             top : (CFG.y || (window.innerHeight - CFG.height) / 2) + "px"
                         });
+                        if (CFG.hasCloseBtn) {
+                            var closeBtn = $('<span class="window_closeBtn">x</span>');
+                            closeBtn.appendTo(boundingBox);
+                            closeBtn.click(function() {
+                                boundingBox.remove();
+                            });
+                        }
+                        if (CFG.skinClassName) {
+                            boundingBox.addClass(CFG.skinClassName);
+                        }
         },
         confirm : function() {
 
