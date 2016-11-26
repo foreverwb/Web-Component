eff4ed8b43d52dd5d8d8002a8325834132ad60a2
foreverwb <504543582@qq.com>
2016-11-26 17:32:05 +0800

    change alert size

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ require.config({
 
 require(['jquery', 'window'], function($, w) {
     $("#a").click(function() {
-         new w.Window().alert("Hello");
+         new w.Window().alert("Hello", function() {
+            console.log("you click the button");
+         },
+         {
+            width : 300,
+            height : 150,
+            y : 50
+         });
     });
 });
--- js/window.js
+++ js/window.js
@@ -1,13 +1,29 @@
 define(['jquery'], function($) {
     function Window() {
-
+        this.cfg = {
+            width : 500,
+            height : 300
+        };
     }
 
     Window.prototype = {
-        alert : function(content) {
+        alert : function(content, handler, cfg) {
                         var boundingBox = $('<div class="window_boundingBox"></div>');
                         boundingBox.appendTo("body");
                         boundingBox.html(content);
+                        var btn = $('<input type="button" value="确定">');
+                        btn.appendTo(boundingBox);
+                        btn.click(function() {
+                            handler && handler();
+                            boundingBox.remove();
+                        });
+                        $.extend(this.cfg, cfg);
+                        boundingBox.css({
+                            width : this.cfg.width + "px",
+                            height : this.cfg.height + "px",
+                            left : (this.cfg.x || (window.innerWidth - this.cfg.width) / 2) + "px",
+                            top : (this.cfg.y || (window.innerHeight - this.cfg.height) / 2) + "px"
+                        });
         },
         confirm : function() {
 
